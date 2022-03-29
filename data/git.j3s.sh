608d0b90e743fe7c7a510b66f0d23fa824c11858
Jakub Wilk <jwilk@jwilk.net>
2022-03-29 12:05:47 +0200

    data/git.j3s.sh: add.

---
+++ data/git.j3s.sh
@@ -0,0 +1,7 @@
+[rules]
+https://git.j3s.sh/git/* = https://git.j3s.sh/$repo/commit/$commit
+
+[tests]
+https://git.j3s.sh/git/poop.is = https://git.j3s.sh/poop.is/commit/372cf8c99e4b093d678b090c3b4e91b0a6f079f8
+
+# vim:ft=dosini
