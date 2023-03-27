d7ca7f7f5517ff5975bc0cb092922da2ec20118f
Jakub Wilk <jwilk@jwilk.net>
2023-03-27 09:44:13 +0200

    data/git.sakamoto.pl: add.

---
+++ data/git.sakamoto.pl
@@ -0,0 +1,7 @@
+[rules]
+https://git.sakamoto.pl/* = https://git.sakamoto.pl/$repo/commit/$commit
+
+[tests]
+https://git.sakamoto.pl/laudom/haruhi-dl = https://git.sakamoto.pl/laudom/haruhi-dl/commit/0526e2add4c263209cad55347efa9a2dfe6c3fa6
+
+# vim:ft=dosini
