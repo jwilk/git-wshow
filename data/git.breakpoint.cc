02356d46a6f910e03c77c27457ef2a7e9aaa0ee4
Jakub Wilk <jwilk@jwilk.net>
2024-03-07 17:02:33 +0100

    data/git.breakpoint.cc: add.

---
+++ data/git.breakpoint.cc
@@ -0,0 +1,9 @@
+[rules]
+https://git.breakpoint.cc/git/* = https://git.breakpoint.cc/cgit/$repo.git/commit/?id=$commit
+git://git.breakpoint.cc/* = https://git.breakpoint.cc/cgit/$repo.git/commit/?id=$commit
+
+[tests]
+https://git.breakpoint.cc/git/fw/tcprdr.git = https://git.breakpoint.cc/cgit/fw/tcprdr.git/commit/?id=bcaccdb25cc529eb29573e3a81203f8c51a7ea4b
+git://git.breakpoint.cc/fw/tcprdr.git = https://git.breakpoint.cc/cgit/fw/tcprdr.git/commit/?id=bcaccdb25cc529eb29573e3a81203f8c51a7ea4b
+
+# vim:ft=dosini
